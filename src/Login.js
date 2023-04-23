b1583029d0db63d4e6f81238b35d94ea4f11f31a
Amit Tiwari <amittiwari@Amits-MacBook-Pro.local>
2023-04-24 00:48:52 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import API from './api';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await API.get('authentication/login/', {
+        params: {
+          username: username,
+          password: password,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Signup.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import API from './api';
+
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  console.log(API)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await API.post('authentication/signup/', {
+        username,
+        password,
+        email,
+      });
+      console.log(response.data);
+      alert('Signup successful!');
+      setUsername('');
+      setPassword('');
+      setEmail('');
+    } catch (error) {
+      console.error(error);
+      alert('Signup failed!');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
